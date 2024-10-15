a09cad94978c7c7b9f84ee3faf96a9ef34637d5c
hulys <hulyssesmaga@gmail.com>
2024-10-15 19:48:59 -0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API funcionando!');
+});
+
+app.listen(3000, () => {
+    console.log('API rodando na porta 3000');
+});
