432b665d7fc264145e761df8516d5a6475936bd0
hulys <hulyssesmaga@gmail.com>
2024-10-16 15:37:55 -0300

    finalizando layout da tela de login e adicionando responsividade

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,12 +1,81 @@
 import Logo from '../../assets/logo/logo.svg';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+    emailAdress: z.string()
+        .nonempty("O campo e-mail é obrigatório.")
+        .email("Por favor, insira um e-mail válido."),
+    password: z.string()
+        .nonempty("O campo senha é obrigatório.")
+});
 
 export const Login = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            emailAdress: '',
+            password: ''
+        }
+    });
+
+    const handleSubmit = () => { };
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            <img className="w-48 mb-5" src={Logo} alt="Logo Khiv" />
-            <div className="bg-white p-7 rounded-sm shadow-md w-96">
+        <main className="flex min-h-screen flex-col items-center justify-center p-5 sm:p-10 md:p-24">
+            <img className="w-32 sm:w-40 md:w-48 mb-5" src={Logo} alt="Logo Khiv" />
+            <div className="bg-white p-7 rounded-sm shadow-md w-full max-w-400">
                 <h2 className="text-2xl font-bold text-blue-900">Login</h2>
-                <p className="text-sm text-gray-500 mb-6">Digite seus dados de acesso</p>
+                <p className="text-sm text-gray-500 mb-5">Digite seus dados de acesso</p>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(handleSubmit)}
+                        className="w-full">
+                        <FormField
+                            control={form.control}
+                            name="emailAdress"
+                            render={({ field }) => {
+                                return <FormItem>
+                                    <FormLabel className="font-bold">E-mail</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            className="bg-whiteF"
+                                            placeholder="Digite seu e-mail"
+                                            type="email" />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            }}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => {
+                                return <FormItem className="mt-5">
+                                    <FormLabel className="font-bold">Senha</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            className="bg-whiteF"
+                                            placeholder="Digite sua senha"
+                                            type="password" />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            }}
+                        />
+                        <Button
+                            type="submit"
+                            className="bg-orange hover:bg-orangeHover font-bold w-full mt-5">
+                            Entrar
+                        </Button>
+                    </form>
+                </Form>
             </div>
         </main>
     );
