6fa4f325d98770875cae00be1cf73b92291977ee
hulys <hulyssesmaga@gmail.com>
2024-10-16 10:51:17 -0300

    centralizando login

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -2,7 +2,7 @@ import Logo from '../../assets/logo/logo.svg';
 
 export const Login = () => {
     return (
-        <main className="flex min-h-screen flex-col items-center p-24">
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <img className="w-48 mb-5" src={Logo} alt="Logo Khiv" />
             <div className="bg-white p-7 rounded-sm shadow-md w-96">
                 <h2 className="text-2xl font-bold text-blue-900">Login</h2>
