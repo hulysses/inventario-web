5715431f779a6195e54c783d8a09052939ea9fe5
Kauã Heidemann Santos <104318673+khsantos@users.noreply.github.com>
2024-10-21 14:44:57 -0300

    added sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
 import { Login } from "./pages/login";
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ClientTable } from "@/pages/clientTables";
+import { AppSidebar } from "@/components/ui/appSidebar"
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
 
-export function App() {
+export function App({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Provider store={store}>
-        <Login />
+        <Router>
+          {/* Conteúdo fixo entre páginas */}
+
+          <SidebarProvider>
+            <AppSidebar />
+            <main>
+              <SidebarTrigger />
+              {children}
+            </main>
+          </SidebarProvider>
+
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="clientTable" element={<ClientTable />} />
+          </Routes>
+        </Router>
       </Provider>
     </>
   )
