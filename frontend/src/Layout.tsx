b36d89a0dc43ab2b8a246286a88ee5e8ce2070a4
hulys <hulyssesmaga@gmail.com>
2024-10-22 11:29:26 -0300

    melhorando front do menu

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,15 +1,17 @@
-import { Outlet } from 'react-router-dom';
-import { AppSidebar } from './components/app-sidebar';
-import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar';
+// import { Outlet } from 'react-router-dom';
+// import { AppSidebar } from './components/app-sidebar';
+// import { SidebarInset, SidebarProvider, SidebarTrigger } from './components/ui/sidebar';
+// import { Separator } from './components/ui/separator';
+// import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from './components/ui/breadcrumb';
 
-export function Layout() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main className='w-full'>
-        <SidebarTrigger />
-        <Outlet />
-      </main>
-    </SidebarProvider>
-  );
-}
+// export function Layout() {
+//   return (
+//     <SidebarProvider>
+//       <AppSidebar />
+//       <main className='w-full'>
+//         <SidebarTrigger />
+//         <Outlet />
+//       </main>
+//     </SidebarProvider>
+//   );
+// }
--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,9 +1,12 @@
-import { useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { Users, Home, SmilePlus, ShoppingBasket, ShoppingCart, Truck, ArrowLeftRight, LogOut } from "lucide-react";
 import Logo from '../assets/logo/logoBranca.svg';
+import { Separator } from './ui/separator';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from './ui/breadcrumb';
 import {
     Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent,
-    SidebarMenu, SidebarMenuButton, SidebarMenuItem,
+    SidebarInset, SidebarMenu, SidebarMenuButton, SidebarMenuItem,
+    SidebarProvider, SidebarTrigger,
 } from "@/components/ui/sidebar";
 
 const items = [
@@ -53,36 +56,62 @@ export function AppSidebar() {
     }
 
     return (
-        <Sidebar>
-            <div className="h-140 bg-blue flex justify-center items-center">
-                <img src={Logo} alt="Logo" />
-            </div>
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupContent>
-                        <SidebarMenu className="gap-7">
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <button
-                                            className="text-xl text-blue flex items-center"
-                                            onClick={() => navigate(item.url)} >
-                                            <item.icon />
-                                            <span>{item.title}</span>
+        <SidebarProvider>
+            <Sidebar collapsible="icon" className='bg-white'>
+                <div className="h-140 bg-blue flex justify-center items-center">
+                    <img src={Logo} alt="Logo" />
+                </div>
+                <SidebarContent>
+                    <SidebarGroup>
+                        <SidebarGroupContent>
+                            <SidebarMenu className="gap-7">
+                                {items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <button
+                                                className="text-xl text-blue flex items-center"
+                                                onClick={() => navigate(item.url)} >
+                                                <item.icon />
+                                                <span>{item.title}</span>
+                                            </button>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                </SidebarContent>
+                <SidebarFooter>
+                    <a onClick={handleLogout} className="flex justify-center items-center text-xl text-red-800 cursor-pointer">
+                        <LogOut />
+                        <span>Sair</span>
+                    </a>
+                </SidebarFooter>
+            </Sidebar>
+            <main className='w-full'>
+                <SidebarInset>
+                    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                        <div className="flex items-center gap-2 px-4">
+                            <SidebarTrigger className="-ml-1" />
+                            <Separator orientation="vertical" className="mr-2 h-4" />
+                            <Breadcrumb>
+                                <BreadcrumbList>
+                                    <BreadcrumbItem className="hidden md:block">
+                                        <button onClick={() => navigate('/home')}>
+                                            Khiv
                                         </button>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-            <SidebarFooter>
-                <a onClick={handleLogout} className="flex justify-center items-center text-xl text-red-800 cursor-pointer">
-                    <LogOut />
-                    <span>Sair</span>
-                </a>
-            </SidebarFooter>
-        </Sidebar>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbSeparator className="hidden md:block" />
+                                    <BreadcrumbItem>
+                                        <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                                    </BreadcrumbItem>
+                                </BreadcrumbList>
+                            </Breadcrumb>
+                        </div>
+                    </header>
+                </SidebarInset>
+                <Outlet />
+            </main>
+        </SidebarProvider>
     );
 }
