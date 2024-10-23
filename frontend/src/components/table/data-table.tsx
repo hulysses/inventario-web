3fec3d5a9c36b4bc8576d41df087b871ca1ad40a
Kauã Heidemann Santos <104318673+khsantos@users.noreply.github.com>
2024-10-23 12:39:14 -0300

    mais modificiações na tabela

--- frontend/src/components/table/data-table.tsx
+++ frontend/src/components/table/data-table.tsx
@@ -2,6 +2,7 @@ import { ColumnDef, flexRender, getCoreRowModel, useReactTable, VisibilityState 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from "@/components/ui/table";
 import React from "react";
 import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -58,7 +59,10 @@ export function DataTable<TData, TValue>({
                                         {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                     </TableCell>
                                 ))}
-                                <Button type="button" className="border-solid border-2 border-gray-200" variant={'ghost'}>Mais detalhes...</Button>
+                                <Button type="button" className="border-solid border-2 border-gray-200" variant={'ghost'}>
+                                    Mais detalhes
+                                    <ArrowRight className="ml-3 w-5" />
+                                </Button>
                             </TableRow>
                         ))
                     ) : (
--- frontend/src/pages/client/index.tsx
+++ frontend/src/pages/client/index.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/sheet"
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { Filter, Search } from "lucide-react";
 
 const dataAtual = new Date();
 const dataFormatada = format(dataAtual, 'dd/MM/yyyy');
@@ -50,8 +51,15 @@ export const ClientTable = () => {
 
             <div className="flex items-center space-x-4">
                 <h1 className="font-bold text-2xl mt-20 -ml-0 flex mx-auto">Clientes <p className="ml-5 text-gray-400">2</p></h1>
-                <input type="text" placeholder="Procurar" className="border rounded px-2 py-1 mt-20 ml-16" />
-                <input type="text" placeholder="Filtrar" className="border rounded px-2 py-1 mt-20 ml-16" />
+                <div className="flex mx-auto mt-20">
+                    <Search className="mt-1 mr-2" />
+                    <input type="text" placeholder="Procurar" className="border rounded px-2 py-1 " />
+                </div>
+                <div className="mt-20 ml-10 flex">
+                    <Filter className="mt-1 mr-2" />
+                    <input type="text" placeholder="Filtrar" className="border rounded px-2 py-1" />
+                </div>
+
 
                 <Sheet>
                     <SheetTrigger type="button" className="border rounded-sm
