977c58ce8c2cf2064cb6f19f1a9bbf14d561e9e2
Kauã Heidemann Santos <104318673+khsantos@users.noreply.github.com>
2024-10-26 00:10:33 -0300

    começo de mudancas no backend para envio de cliente para o banco

--- frontend/src/components/sidebarSheet/index.tsx
+++ frontend/src/components/sidebarSheet/index.tsx
@@ -10,8 +10,38 @@ import {
 } from "@/components/ui/sheet"
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "sonner";
+import { Toaster } from "../ui/sonner";
 
-export const SidebarSheet = () => {
+export default function SidebarSheet() {
+    const baseUrl = 'http://localhost:3000/clients'
+
+    const [nome, setNome] = useState('');
+    const [cpfCnpj, setCpfCnpj] = useState('');
+    const [endereco, setEndereco] = useState('');
+    const [contato, setContato] = useState('');
+    const [clientAdded, setClientAdded] = useState<boolean | null>(null);
+
+    async function handleSubmit(event: any) {
+        event.preventDefault();
+        setClientAdded(false);
+
+        axios.post(baseUrl, {
+            nome: nome,
+            cpf_cnpj: cpfCnpj,
+            endereco: endereco,
+            contato: contato
+        }).then((response) => {
+            console.log(response.data);
+            setClientAdded(true);
+        }).catch((error) => {
+            setClientAdded(false);
+            console.log("Erro ao inserir cliente: ", error);
+        })
+    }
+
     return (
         <Sheet>
             <SheetTrigger type="button" className="rounded-sm
@@ -27,43 +57,46 @@ export const SidebarSheet = () => {
                             <Label htmlFor="nome" className="text-right">
                                 Nome
                             </Label>
-                            <Input id="nome" value="" placeholder="Pedro Duarte" className="col-span-3 border-solid border-2 border-blue" />
+                            <Input id="nome" value={nome} onChange={(e) => setNome(e.target.value)} placeholder="Pedro Duarte" className="col-span-3 border-solid border-2 border-blue" />
                         </div>
                         <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="e-mail" className="text-right">
-                                E-mail
-                            </Label>
-                            <Input id="e-mail" value="" placeholder="peduarte@hotmail.com" className="col-span-3 border-solid border-2 border-blue" />
-                        </div>
-
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="e-mail" className="text-right">
+                            <Label className="text-right">
                                 CPF/CNPJ
                             </Label>
-                            <Input id="e-mail" placeholder="782.321.243-12" className="col-span-3 border-solid border-2 border-blue" />
+                            <Input value={cpfCnpj} onChange={(e) => setCpfCnpj(e.target.value)} placeholder="123-456-789-11" className="col-span-3 border-solid border-2 border-blue" />
                         </div>
 
                         <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="e-mail" className="text-right">
+                            <Label className="text-right">
                                 Contato
                             </Label>
-                            <Input id="e-mail" value="" placeholder="(99)9 9999 9999" className="col-span-3 border-solid border-2 border-blue" />
+                            <Input value={contato} onChange={(e) => setContato(e.target.value)} placeholder="(99)9 9999 9999" className="col-span-3 border-solid border-2 border-blue" />
                         </div>
 
                         <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="e-mail" className="text-right">
+                            <Label className="text-right">
                                 Endereço
                             </Label>
-                            <Input id="e-mail" value="" placeholder="Rua Dois 243" className="col-span-3 border-solid border-2 border-blue" />
+                            <Input id="e-mail" value={endereco} onChange={(e) => setEndereco(e.target.value)} placeholder="Rua Dois 243" className="col-span-3 border-solid border-2 border-blue" />
                         </div>
 
-                        <Button type="submit" className="hover:bg-orangeHover bg-orange">Confirmar</Button>
+                        <Button type="submit" onClick={handleSubmit} className="hover:bg-orangeHover bg-orange">Confirmar</Button>
+
+                        <Toaster />
+
+                        {clientAdded !== null && (
+                            <div>
+                                {clientAdded
+                                    ? toast("Usuário inserido com sucesso.")
+                                    : toast("Falha ao inserir usuário.")
+                                }
+                            </div>
+                        )}
+                        <Toaster />
+
                     </div>
                 </SheetHeader>
             </SheetContent>
-        </Sheet>
+        </Sheet >
     )
-
-
-
 }
