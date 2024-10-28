42a4a4e672a3ff28dc823ca85b4d49cccbde8ca5
hulys <hulyssesmaga@gmail.com>
2024-10-27 21:51:39 -0300

    corrigindo layout das acoes

--- frontend/src/components/table/columnsTable/columnsTableSupplier.tsx
+++ frontend/src/components/table/columnsTable/columnsTableSupplier.tsx
@@ -2,13 +2,10 @@ import { Supplier } from "@/types/Supplier";
 import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
 import { formatarCNPJ } from "@/helpers/registerHelper";
-import { useSuppliers } from "../../../hooks/useSuppliers";
 import { formatarTelefone } from "@/helpers/registerHelper";
 import { ArrowUpDown, EllipsisVertical } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "@radix-ui/react-dialog";
-import { DialogFooter, DialogHeader } from "@/components/ui/dialog";
-import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenu, DropdownMenuItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
 
 export const columns = (handleEdit: (supplier: Supplier) => void): ColumnDef<Supplier>[] => [
     {
@@ -100,39 +97,21 @@ export const columns = (handleEdit: (supplier: Supplier) => void): ColumnDef<Sup
             const data = row.original;
 
             return (
-                <div>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="h-8 w-8 p-0">
-                                <span className="sr-only">Open menu</span>
-                                <EllipsisVertical className="h-4 w-4" />
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            <DropdownMenuItem onClick={() => {handleEdit(data)}}>
-                                Editar
-                            </DropdownMenuItem>
-                            {/* <Dialog>
-                                <DialogTrigger asChild>
-                                    <Button variant="outline">Excluir</Button>
-                                </DialogTrigger>
-                                <DialogContent>
-                                    <DialogHeader>
-                                        <DialogTitle>Excluir</DialogTitle>
-                                        <DialogDescription>
-                                            <div className="flex items-center text-green-600">
-                                                Deseja prosseguir com a exclusão?
-                                            </div>
-                                        </DialogDescription>
-                                    </DialogHeader>
-                                    <DialogFooter>
-                                        <Button type="submit">Save changes</Button>
-                                    </DialogFooter>
-                                </DialogContent>
-                            </Dialog> */}
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <EllipsisVertical className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => handleEdit(data)} >
+                            Editar
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => handleEdit(data)}>
+                            Excluir
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             )
         },
     },
