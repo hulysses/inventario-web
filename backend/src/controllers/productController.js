6d87e74b77cbb5cc09c576a14fcfd34bb1bf914f
hulysses <hulyssesmaga@gmail.com>
2024-11-15 16:24:24 -0300

    feat: add listProductsSupplier function and improve product controller structure

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,45 +1,61 @@
-import { insertProduct, listProduct, updateProduct, deleteProduct } from "../database/services/productService.js";
+import {
+  insertProduct,
+  listProduct,
+  updateProduct,
+  deleteProduct,
+} from "../database/services/productService.js";
 
 export const registerProduct = (req, res) => {
-    const { nome, descricao, preco, quantidade, imagem, supplier_id } = req.body;
+  const { nome, descricao, preco, quantidade, imagem, supplier_id } = req.body;
 
-    if (insertProduct(nome, descricao, preco, quantidade, imagem, supplier_id)) {
-        res.status(201).json({ message: 'Produto cadastrado com sucesso' });
-    } else {
-        res.status(400).json({ message: 'Erro ao cadastrar produto.' });
-    }
-}
+  if (insertProduct(nome, descricao, preco, quantidade, imagem, supplier_id)) {
+    res.status(201).json({ message: "Produto cadastrado com sucesso" });
+  } else {
+    res.status(400).json({ message: "Erro ao cadastrar produto." });
+  }
+};
 
 export const listProducts = (req, res) => {
-    try {
-        const products = listProduct();
-        res.status(200).json(products);
-    } catch (error) {
-        console.error('Erro ao listar produtos:', error);
-        res.status(500).json({ message: 'Erro ao listar produtos.' });
-    }
-}
+  try {
+    const products = listProduct();
+    res.status(200).json(products);
+  } catch (error) {
+    console.error("Erro ao listar produtos:", error);
+    res.status(500).json({ message: "Erro ao listar produtos." });
+  }
+};
+
+export const listProductsSupplier = (req, res) => {
+  try {
+    const products = listProductWithSuppliers();
+    res.status(200).json(products);
+  } catch (error) {
+    console.error("Erro ao listar produtos:", error);
+    res.status(500).json({ message: "Erro ao listar produtos." });
+  }
+};
 
 export const updateProducts = (req, res) => {
-    try {
-        const { id } = req.query;
-        const { nome, descricao, preco, quantidade, imagem, supplier_id } = req.body;
+  try {
+    const { id } = req.query;
+    const { nome, descricao, preco, quantidade, imagem, supplier_id } =
+      req.body;
 
-        updateProduct(id, nome, descricao, preco, quantidade, imagem, supplier_id);
-        res.status(200).json({ message: 'Produto atualizado com sucesso' });
-    } catch (error) {
-        console.error('Erro ao atualizar produtos:', error);
-        res.status(500).json({ message: 'Erro ao atualizar produto.' });
-    }
-}
+    updateProduct(id, nome, descricao, preco, quantidade, imagem, supplier_id);
+    res.status(200).json({ message: "Produto atualizado com sucesso" });
+  } catch (error) {
+    console.error("Erro ao atualizar produtos:", error);
+    res.status(500).json({ message: "Erro ao atualizar produto." });
+  }
+};
 
 export const deleteProducts = (req, res) => {
-    try {
-        const { id } = req.query;
-        deleteProduct(id);
-        res.status(200).json({ message: 'Produto deletado com sucesso.' });
-    } catch (error) {
-        console.error('Erro ao deletar produto:', error);
-        res.status(500).json({ message: 'Erro ao deletar produto.' });
-    }
-}
+  try {
+    const { id } = req.query;
+    deleteProduct(id);
+    res.status(200).json({ message: "Produto deletado com sucesso." });
+  } catch (error) {
+    console.error("Erro ao deletar produto:", error);
+    res.status(500).json({ message: "Erro ao deletar produto." });
+  }
+};
